b2b60a3f8da86e19c2a59a18ab4bff7604c18c8f
Ankur Sheel <ankur.sheel@gmail.com>
2021-12-01 19:08:29 +1300

    Add Day1 part1

---
+++ AdventOfCode2020.Tests/Day1/SonarSweepTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2020.Day1;
+using Xunit;
+using xUnitHelpers;
+
+namespace AdventOfCode2020.Tests.Day1
+{
+    public class SonarSweepTests
+    {
+        private readonly SonarSweep _sonarSweep;
+
+        public SonarSweepTests()
+        {
+            _sonarSweep = new SonarSweep();
+        }
+
+        [Theory]
+        [JsonFileData("Day1/testData.json", "Part1", typeof(List<int>), typeof(int))]
+        public void GetTotalFuelRequirementReturnCorrectTotalFuelRequirement(List<int> data, int expectedResult)
+        {
+            var result = _sonarSweep.CountDepthMeasurementIncreases(data);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day1/SonarSweep.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day1
+{
+    public class SonarSweep
+    {
+        public int CountDepthMeasurementIncreases(List<int> data)
+        {
+            var numberOfIncreases = 0;
+            var previousMeasurement = data.First();
+
+            for (var i = 1; i < data.Count; i++)
+            {
+                var currentMeasurement = data[i];
+
+                if (currentMeasurement > previousMeasurement)
+                {
+                    numberOfIncreases++;
+                }
+
+                previousMeasurement = currentMeasurement;
+            }
+
+            return numberOfIncreases;
+        }
+    }
+}
