be82b95af2b231f98830bb00e798e8fb5b825d06
Ankur Sheel <ankur.sheel@gmail.com>
2021-12-03 00:31:26 +1300

    add day 2 part 2

--- AdventOfCode2020.Tests/Day2/DiveTests.cs
+++ AdventOfCode2020.Tests/Day2/DiveTests.cs
@@ -21,5 +21,13 @@ namespace AdventOfCode2020.Tests.Day2
             var result = _dive.GetPositionMultiplicationResult(data);
             Assert.Equal(expectedResult, result);
         }
+
+        [Theory]
+        [JsonFileData("Day2/testData.json", "Part2", typeof(List<string>), typeof(int))]
+        public void GetPositionAimMultiplicationResult(List<string> data, int expectedResult)
+        {
+            var result = _dive.GetPositionAimMultiplicationResult(data);
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
--- AdventOfCode2020/Day2/Dive.cs
+++ AdventOfCode2020/Day2/Dive.cs
@@ -32,5 +32,35 @@ namespace AdventOfCode2020.Day2
             }
             return horizontalMovement * verticalMovement;
         }
+
+        public int GetPositionAimMultiplicationResult(IReadOnlyList<string> data)
+        {
+            var horizontalMovement = 0;
+            var verticalMovement = 0;
+            var aim = 0;
+
+            foreach (var movement in data)
+            {
+                var split = movement.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var direction = split[0];
+                int.TryParse(split[1], out var units);
+
+                if (direction == "forward")
+                {
+                    horizontalMovement += units;
+                    verticalMovement += (aim * units);
+                }
+
+                else if (direction == "down")
+                {
+                    aim += units;
+                }
+                if (direction == "up")
+                {
+                    aim -= units;
+                }
+            }
+            return horizontalMovement * verticalMovement;
+        }
     }
 }
