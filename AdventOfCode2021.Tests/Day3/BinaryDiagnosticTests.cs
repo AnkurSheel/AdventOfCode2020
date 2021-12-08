a9c8ef389ad32aeba02a0fc3331ed3665777f201
Ankur Sheel <ankur.sheel@gmail.com>
2021-12-08 23:42:21 +1300

    add Day 3 Part 1

---
+++ AdventOfCode2021.Tests/Day3/BinaryDiagnosticTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2021.Day3;
+using Xunit;
+using xUnitHelpers;
+
+namespace AdventOfCode2021.Tests.Day3
+{
+    public class BinaryDiagnosticTests
+    {
+        private readonly BinaryDiagnostic _binaryDiagnostic;
+
+        public BinaryDiagnosticTests()
+        {
+            _binaryDiagnostic = new BinaryDiagnostic();
+        }
+
+        [Theory]
+        [JsonFileData(
+            "Day3/testData.json",
+            "Part1",
+            typeof(List<string>),
+            typeof(int))]
+        public void GetPowerConsumption(List<string> data, int expectedResult)
+        {
+            var result = _binaryDiagnostic.GetPowerConsumption(data);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day3/BinaryDiagnostic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2021.Day3
+{
+    public class BinaryDiagnostic
+    {
+        public int GetPowerConsumption(IReadOnlyList<string> data)
+        {
+            var numberOfBits = data[0].Length;
+            var numberOfZeros = new int[numberOfBits];
+            var numberOfOnes = new int[numberOfBits];
+
+            foreach (var binaryNumber in data)
+            {
+                for (var i = 0; i < binaryNumber.Length; i++)
+                {
+                    switch (binaryNumber[i])
+                    {
+                        case '0':
+                            numberOfZeros[i]++;
+                            break;
+                        case '1':
+                            numberOfOnes[i]++;
+                            break;
+                    }
+                }
+            }
+
+            var gammaRate = new StringBuilder();
+            var epsilonRate = new StringBuilder();
+
+            for (var i = 0; i < numberOfBits; i++)
+            {
+                if (numberOfZeros[i] > numberOfOnes[i])
+                {
+                    gammaRate.Append('0');
+                    epsilonRate.Append('1');
+                }
+                else
+                {
+                    gammaRate.Append('1');
+                    epsilonRate.Append('0');
+                }
+            }
+
+            var gammaRateInDecimal = Convert.ToInt32(gammaRate.ToString(), 2);
+            var epsilonRateInDecimal = Convert.ToInt32(epsilonRate.ToString(), 2);
+            return gammaRateInDecimal * epsilonRateInDecimal;
+        }
+    }
+}
